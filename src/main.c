a3720886e6199e71d0712eacbbe55068ea2fd37c
chenota <achenot@mines.edu>
2025-02-01 23:32:45 -0700

    feat: Add text and key processing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <argp.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 #include "des.h"
 
 const char *argp_program_version = "des 1.0.0";
@@ -10,13 +11,10 @@ static char args_doc[] = "TEXT KEY";
 static struct argp_option options[] = {
     { "encrypt", 'e', 0, 0, "Run in encrypt mode" },
     { "decrypt", 'd', 0, 0, "Run in decrypt mode" },
-    { "hex", 'h', 0, 0, "Use hexadecimal input" },
-    { "ascii", 'a', 0, 0, "Use ASCII input" }
 };
 
 struct arguments {
     enum { ENCRYPT, DECRYPT, NONE } mode;
-    enum { HEX, ASCII } input;
     char *args[2];
 };
 
@@ -25,8 +23,6 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     switch (key) {
         case 'e' : arguments->mode = ENCRYPT; break;
         case 'd' : arguments->mode = DECRYPT; break;
-        case 'h' : arguments->input = HEX; break;
-        case 'a' : arguments->input = ASCII; break;
         case ARGP_KEY_ARG:
             if(state->arg_num >= 2) argp_usage(state);
             arguments->args[state->arg_num] = arg;
@@ -51,26 +47,23 @@ int main(int argc, char *argv[]) {
     // Do argument parsing
     struct arguments arguments;
     arguments.mode = NONE;
-    arguments.input = ASCII;
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
     // Get pointers to text and key
-    char *text = arguments.args[0];
-    char *key = arguments.args[1];
+    char *inputText = arguments.args[0];
+    char *keyText = arguments.args[1];
     // Get length of text and key (should be no more than 16 bits, so restrict to a couple over that)
-    size_t textLen = strnlen(text, 18);
-    size_t keyLen = strnlen(key, 18);
-    // Validate length of text and key
-    switch(arguments.input) {
-        case ASCII:
-            if(textLen > 8 || keyLen > 8) {
-                perror("Text and Key length must be no more than eight characters");
-                exit(1);
-            }
-        case HEX:
-            if(textLen > 16 || keyLen > 16) {
-                perror("Text and Key length must be no more than sixteen characters");
-                exit(1);
-            }
+    size_t textLen = strnlen(inputText, 18);
+    size_t keyLen = strnlen(keyText, 18);
+    // Validate length
+    if(textLen > 16 || keyLen > 16) {
+        perror("Text and Key length must be no more than sixteen characters");
+        exit(1);
     }
+    // Allocate text and key blocks
+    uint64_t text = 0;
+    uint64_t key = 0;
+    // Fill blocks
+    text = strtol(inputText, NULL, 16);
+    key = strtol(keyText, NULL, 16);
     return 0;
 }
